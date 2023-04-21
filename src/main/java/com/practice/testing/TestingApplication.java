2559041e994669475e1e357ef9d5be602c61b2bc
surajdonge123 <surajdonge143@gmail.com>
2023-04-21 22:42:38 +0530

    By suraj

---
+++ src/main/java/com/practice/testing/TestingApplication.java
@@ -0,0 +1,23 @@
+package com.practice.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingApplication.class, args);
+
+	System.out.println("Hellow world");
+	
+	}
+
+ public String stashmethod() {
+	 
+	 return "hi hello";
+	 
+ }
+		
+	
+}
